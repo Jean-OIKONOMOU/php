8dc27bcbdff1829424ee6fd61a3a69527d2a4c91
Jean-Adrian OIKONOMOU <Jean.oikonomou@gmail.com>
2019-03-11 16:40:57 +0100

    WIP

---
+++ fonctions.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+/* PHP-EX-VARIABLES */
+/* EXERCICE 1 */
+function writeMsg() {
+$a = true;
+  if (is_bool($a) === true) {
+    echo "True" . "<br />";
+  }
+}
+writeMsg();
+/* EXERCICE 2 */
+function writeMsg1($arg1) {
+echo "$arg1.<br>";
+}
+writeMsg1("azer");
+writeMsg1("test");
+
+/* EXERCICE 3 */
+function writeMsg2($arg1, $arg2) {
+echo $arg1 . " " . $arg2 . "<br />";
+}
+writeMsg2("Bob", "LENON");
+writeMsg2("Caterina", "THEMISTOCLES");
+
+/* EXERCICE 4 */
+
+
+/* EXERCICE 5 */
+
+
+/* EXERCICE 6 */
+
+
+/* EXERCICE 7 */
+
+/* EXERCICE 8 */
+
+ ?>
+</body>
+</html>
