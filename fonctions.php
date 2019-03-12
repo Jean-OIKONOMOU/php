fa996a62676468d24cf2580da4424c39fe2c2d5a
Jean-Adrian OIKONOMOU <Jean.oikonomou@gmail.com>
2019-03-12 12:11:15 +0100

    WIP

--- fonctions.php
+++ fonctions.php
@@ -1,7 +1,15 @@
 <!DOCTYPE html>
 <html>
 <body>
+
 <?php
+if (!ini_get('display_errors')) {
+    ini_set('display_errors', '1');
+}
+
+echo ini_get('display_errors');
+ini_set('log_errors', 1);
+  ini_set('display_errors', 1);
 /* PHP-EX-VARIABLES */
 /* EXERCICE 1 */
 function writeMsg() {
@@ -30,26 +38,51 @@ function valueCompare($arg1, $arg2) {
 settype($arg1, "integer");
 settype($arg2, "integer");
 if ($arg1 > $arg2) {
-    echo "Le premier nombre est plus grand.";
+    echo "Le premier nombre est plus grand. <br />";
   } elseif ($arg1 < $arg2) {
-    echo "Le premier nombre est plus petit.";
+    echo "Le premier nombre est plus petit. <br />";
   } else {
-    echo "Les deux nombres sont identiques.";
+    echo "Les deux nombres sont identiques. <br />";
   }
 }
 valueCompare(35, 85);
 /* EXERCICE 5 */
-
-
+function toString(int $arg1, string $arg2) {
+$_string = (string)$arg1;
+print "{$_string} {$arg2} <br />";
+}
+toString(6, "salut");
 /* EXERCICE 6 */
-
+function helloMsg(int $age, string $nom, string $prenom) {
+print "Bonjour {$nom} {$prenom}, tu as {$age} ans. <br />";
+}
+helloMsg(28, "finkelstein", "alex");
 
 /* EXERCICE 7 */
-
+function genreAge(int $age2, string $genre) {
+  $genre = strtolower($genre);
+  if ($genre !== "homme" && $genre !== "femme") {
+echo '<script language="javascript">';
+echo 'alert("Vous ne pouvez mettre que homme ou femme.")';
+echo '</script>';
+} elseif ($genre == "homme" && $age2 > 18) {
+  echo "Vous êtes un homme et vous êtes majeur.<br />";
+  } elseif ($genre == "homme" && $age2 <= 18) {
+  echo "Vous êtes un homme et vous êtes mineur.<br />";
+  } elseif ($genre == "femme" && $age2 > 18) {
+  echo "Vous êtes un femme et vous êtes majeur.<br />";
+} elseif ($genre == "femme" && $age2 <= 18) {
+  echo "Vous êtes un femme et vous êtes mineure. <br />";
+  }
+}
+genreAge(8, "femme");
 
 /* EXERCICE 8 */
-
-
+function makecoffee(int $a = 1, int $b = 2, int $c = 3) {
+  $d = $a + $b + $c;
+  return "The sum is equal to: {$d}.\n";
+}
+echo makecoffee();
  ?>
 </body>
 </html>
